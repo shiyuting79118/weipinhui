e2434d8a762dc1b6105c699b0763bdbaa8ed0162
shiyuting <shiyuting.79118@163.com>
2017-04-01 15:47:59 +0800

    没有git忽略文件

--- config/web.php
+++ config/web.php
@@ -9,7 +9,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => 'dfgregjytu53634531qfvaesrh45643613',
+            'cookieValidationKey' => '',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
