915321076a8589ca0a3f6a4cca0b6be08dde048a
xuanxuan <xuanxuan>
2017-04-17 21:06:27 +0800

    登录页面提交

--- modules/home/views/user/login.php
+++ modules/home/views/user/login.php
@@ -1,3 +1,18 @@
 <div class="login-form">
     <h5>如有帐户，请登录</h5>
+    <div>
+        <input type="text" name="username" placeholder="用户名/手机/邮箱">
+    </div>
+
+    <div>
+        <input type="password" name="password" placeholder="密码">
+    </div>
+
+    <div>
+        <button type="button" class="btn btn-default login-btn">登录</button>
+    </div>
+    <div>
+        <input type="checkbox" checked id="aaaa">记住用户名
+        <div>忘记密码｜免费注册</div>
+    </div>
 </div>
